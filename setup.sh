706bfe1e3f43dd3688e13bd4aa14e666dc28b143
wenduocheng <wenduocheng1478@gmail.com>
2024-11-18 05:53:47 -0500

    update pyproject

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ pip install selene-sdk==0.5.3
 pip install torchmetrics kipoiseq==0.5.2 BioPython pandas Cython
 
 pip install scipy==1.12.0 matplotlib==3.8 pyBigWig
-pip install tensorflow==2.12.0
+pip install tensorflow==2.17.0
 pip install typing_extensions==4.12.2
 pip install torch==2.1.0+cu118 torchvision==0.16.0+cu118 torchaudio==2.1.0+cu118 torchtext==0.16.0 -f https://download.pytorch.org/whl/cu118/torch_stable.html
 pip install numpy==1.26.4 pandas==2.1.4
