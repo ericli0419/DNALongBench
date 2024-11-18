09c2719150f55bb172a3e34862872ee9ae894d77
Wenduo Cheng <wenduoc@lanec1.compbio.cs.cmu.edu>
2024-11-18 05:18:12 -0500

    update

--- setup.sh
+++ setup.sh
@@ -1,25 +1,15 @@
-#!/bin/bash
+# #!/bin/bash
 
-conda create -n dnalongbench python=3.8 
+conda create -n dnalongbench python=3.9
 conda activate dnalongbench
+pip install selene-sdk==0.5.3
 
-pip install tensorflow==2.4.1
-pip install numpy==1.19.2
-
-pip install torch==2.1.0+cu118 torchvision==0.16.0+cu118 torchaudio==2.1.0+cu118 torchtext==0.16.0 -f https://download.pytorch.org/whl/cu118/torch_stable.html
 pip install torchmetrics kipoiseq==0.5.2 BioPython pandas Cython
 
-git clone https://github.com/kathyxchen/selene.git
-pip install selene-sdk
-cd selene
-git checkout custom_target_support
-python setup.py build_ext --inplace
-python setup.py install
-cd ..
-
-
+pip install scipy==1.12.0 matplotlib==3.8 pyBigWig
+pip install tensorflow==2.12.0
+pip install typing_extensions==4.12.2
 pip install torch==2.1.0+cu118 torchvision==0.16.0+cu118 torchaudio==2.1.0+cu118 torchtext==0.16.0 -f https://download.pytorch.org/whl/cu118/torch_stable.html
+pip install numpy==1.26.4 pandas==2.1.4
+pip install natsort pytabix tqdm 
 
-
-pip install natsort pytabix tqdm
-echo "Setup complete. The environment is ready."
